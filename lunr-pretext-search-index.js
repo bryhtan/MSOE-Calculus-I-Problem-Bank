b184066087496337b1ba5725011cf2a11858d6f9
bryhtan <99033719+bryhtan@users.noreply.github.com>
2025-03-08 14:13:41 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Chapter",
   "number": "1",
   "title": "Exam 2 Review",
-  "body": " Exam 2 Review    1. I can find the derivative of a function using the limit definition.    Need to know              2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.    Textbook problems Section 2.7: 49 - 56;  Textbook problems Section 2.8: 12 - 15, 36 - 40    I can find an equation for a tangent line to a function at a point using the derivative.       I can use the graph of a function to determine the behavior of its derivative, including constructing a graph of the dereivative and identifying points of non-differentiability.  Textbook problems Section 2.8: 1 - 11, 41 - 44  I can determine the behavior of a function given the graph of its derivative, including identifying points of non-differentiability.  Problems from review worksheet.  I can find the derivative of algebraic, trigonometric, exponential, logarithmic, and inverse trigonometric functions.                 I can apply basic differentiation rules including power, product, quotient and chain rules to find derivatives of a given function.     I can find higher-order derivatives.  I can find derivatives using implicit differentitation.  I can find derivatives using logarithmic differentiation.  "
+  "body": " Exam 2 Review    1. I can find the derivative of a function using the limit definition.    Need to know              2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.    Textbook problems Section 2.7: 49 - 56;  Textbook problems Section 2.8: 12 - 15, 36 - 40    3. I can find an equation for a tangent line to a function at a point using the derivative.       I can use the graph of a function to determine the behavior of its derivative, including constructing a graph of the dereivative and identifying points of non-differentiability.  Textbook problems Section 2.8: 1 - 11, 41 - 44  I can determine the behavior of a function given the graph of its derivative, including identifying points of non-differentiability.  Problems from review worksheet.  I can find the derivative of algebraic, trigonometric, exponential, logarithmic, and inverse trigonometric functions.                 I can apply basic differentiation rules including power, product, quotient and chain rules to find derivatives of a given function.     I can find higher-order derivatives.  I can find derivatives using implicit differentitation.  I can find derivatives using logarithmic differentiation.  "
 },
 {
   "id": "chapter-testing-2",
@@ -52,7 +52,7 @@ var ptx_lunr_docs = [
   "type": "Objectives",
   "number": "1",
   "title": "",
-  "body": "  2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.    Textbook problems Section 2.7: 49 - 56;  Textbook problems Section 2.8: 12 - 15, 36 - 40    I can find an equation for a tangent line to a function at a point using the derivative.   "
+  "body": "  2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.    Textbook problems Section 2.7: 49 - 56;  Textbook problems Section 2.8: 12 - 15, 36 - 40    3. I can find an equation for a tangent line to a function at a point using the derivative.   "
 },
 {
   "id": "chapter-testing-8",
@@ -116,15 +116,6 @@ var ptx_lunr_docs = [
   "number": "1.10",
   "title": "",
   "body": "  "
-},
-{
-  "id": "backmatter-2",
-  "level": "1",
-  "url": "backmatter-2.html",
-  "type": "Colophon",
-  "number": "",
-  "title": "Colophon",
-  "body": " This book was authored in PreTeXt .  "
 }
 ]
 
