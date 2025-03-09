86a090cec9afc093ba5d08c3558666b64184cfd2
bryhtan <99033719+bryhtan@users.noreply.github.com>
2025-03-09 13:10:42 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Chapter",
   "number": "1",
   "title": "Exam 2 Review",
-  "body": " Exam 2 Review    1. I can find the derivative of a function using the limit definition.    Need to know              2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.    Textbook problems Section 2.7: 49 - 56;  Textbook problems Section 2.8: 12 - 15, 36 - 40    3. I can find an equation for a tangent line to a function at a point using the derivative.       I can use the graph of a function to determine the behavior of its derivative, including constructing a graph of the dereivative and identifying points of non-differentiability.  Textbook problems Section 2.8: 1 - 11, 41 - 44  I can determine the behavior of a function given the graph of its derivative, including identifying points of non-differentiability.  Problems from review worksheet.  I can find the derivative of algebraic, trigonometric, exponential, logarithmic, and inverse trigonometric functions.                 I can apply basic differentiation rules including power, product, quotient and chain rules to find derivatives of a given function.     I can find higher-order derivatives.  I can find derivatives using implicit differentitation.  I can find derivatives using logarithmic differentiation.  "
+  "body": " Exam 2 Review     1. I can find the derivative of a function using the limit definition.   Section 2.8: 21 - 32    Need to know              2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.       2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.   Section 2.7: 49 - 56;   Section 2.8: 12 - 15, 36 - 40       3. I can find an equation for a tangent line to a function at a point using the derivative.   Section 2.7: 5 - 8, 27 - 30;       2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.   Section 2.7: 49 - 56;   Section 2.8: 12 - 15, 36 - 40       3. I can find an equation for a tangent line to a function at a point using the derivative.   Section 2.7: 5 - 8, 27 - 30;          4. I can use the graph of a function to determine the behavior of its derivative, including constructing a graph of the dereivative and identifying points of non-differentiability.   Section 2.8: 1 - 11, 41 - 44;       5. I can determine the behavior of a function given the graph of its derivative, including identifying points of non-differentiability.   Examples given on review day.        6. I can find the derivative of algebraic, trigonometric, exponential, logarithmic, and inverse trigonometric functions.   Section 3.3: 1 - 22;   Section 3.6: 2 - 12, 63, 78                      7. I can apply basic differentiation rules including power, product, quotient and chain rules to find derivatives of a given function.   Section 3.1: 3 - 34;   Section 3.4: 7 - 52             8. I can find higher-order derivatives.   Section 3.4: 53 - 56             9. I am comfortable using various notations for derivatives.       10. I can find derivatives using implicit differentitation.   Section 3.5: 5 - 22             11. I can find derivatives using logarithmic differentiation.   Section 3.6: 45 - 56          12. I can determine when to use appropriate differentiation rules, including when to use the power, quotient, and chain rules, as well as using implicit and logarthmic differentiation.   Chapter 3 Review (p. 270): 1-20, 22-26, 28-33, 35-46, 48, 50, 53-54          "
 },
 {
   "id": "chapter-testing-2",
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Objectives",
   "number": "1",
   "title": "",
-  "body": "  1. I can find the derivative of a function using the limit definition.   "
+  "body": "   1. I can find the derivative of a function using the limit definition.   Section 2.8: 21 - 32   "
 },
 {
   "id": "chapter-testing-4",
@@ -52,41 +52,86 @@ var ptx_lunr_docs = [
   "type": "Objectives",
   "number": "1",
   "title": "",
-  "body": "  2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.    Textbook problems Section 2.7: 49 - 56;  Textbook problems Section 2.8: 12 - 15, 36 - 40    3. I can find an equation for a tangent line to a function at a point using the derivative.   "
+  "body": "  2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.   "
 },
 {
   "id": "chapter-testing-8",
   "level": "2",
   "url": "chapter-testing.html#chapter-testing-8",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.   Section 2.7: 49 - 56;   Section 2.8: 12 - 15, 36 - 40   "
+},
+{
+  "id": "chapter-testing-9",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-9",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   3. I can find an equation for a tangent line to a function at a point using the derivative.   Section 2.7: 5 - 8, 27 - 30;   "
+},
+{
+  "id": "chapter-testing-10",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-10",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   2. I can interpret the derivative as an instantaneous rate of change and as the slope of a tangent line.   Section 2.7: 49 - 56;   Section 2.8: 12 - 15, 36 - 40   "
+},
+{
+  "id": "chapter-testing-11",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-11",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   3. I can find an equation for a tangent line to a function at a point using the derivative.   Section 2.7: 5 - 8, 27 - 30;   "
+},
+{
+  "id": "chapter-testing-12",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-12",
   "type": "Example",
   "number": "1.4",
   "title": "",
   "body": "  "
 },
 {
+  "id": "chapter-testing-13",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-13",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   4. I can use the graph of a function to determine the behavior of its derivative, including constructing a graph of the dereivative and identifying points of non-differentiability.   Section 2.8: 1 - 11, 41 - 44;   "
+},
+{
   "id": "chapter-testing-14",
   "level": "2",
   "url": "chapter-testing.html#chapter-testing-14",
-  "type": "Example",
-  "number": "1.5",
+  "type": "Objectives",
+  "number": "1",
   "title": "",
-  "body": "  "
+  "body": "   5. I can determine the behavior of a function given the graph of its derivative, including identifying points of non-differentiability.   Examples given on review day.    "
 },
 {
   "id": "chapter-testing-15",
   "level": "2",
   "url": "chapter-testing.html#chapter-testing-15",
-  "type": "Example",
-  "number": "1.6",
+  "type": "Objectives",
+  "number": "1",
   "title": "",
-  "body": "  "
+  "body": "   6. I can find the derivative of algebraic, trigonometric, exponential, logarithmic, and inverse trigonometric functions.   Section 3.3: 1 - 22;   Section 3.6: 2 - 12, 63, 78   "
 },
 {
   "id": "chapter-testing-16",
   "level": "2",
   "url": "chapter-testing.html#chapter-testing-16",
   "type": "Example",
-  "number": "1.7",
+  "number": "1.5",
   "title": "",
   "body": "  "
 },
@@ -95,7 +140,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "chapter-testing.html#chapter-testing-17",
   "type": "Example",
-  "number": "1.8",
+  "number": "1.6",
   "title": "",
   "body": "  "
 },
@@ -104,7 +149,16 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "chapter-testing.html#chapter-testing-18",
   "type": "Example",
-  "number": "1.9",
+  "number": "1.7",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-19",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-19",
+  "type": "Example",
+  "number": "1.8",
   "title": "",
   "body": "  "
 },
@@ -113,7 +167,142 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "chapter-testing.html#chapter-testing-20",
   "type": "Example",
+  "number": "1.9",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-21",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-21",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   7. I can apply basic differentiation rules including power, product, quotient and chain rules to find derivatives of a given function.   Section 3.1: 3 - 34;   Section 3.4: 7 - 52   "
+},
+{
+  "id": "chapter-testing-22",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-22",
+  "type": "Example",
   "number": "1.10",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-23",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-23",
+  "type": "Example",
+  "number": "1.11",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-24",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-24",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   8. I can find higher-order derivatives.   Section 3.4: 53 - 56   "
+},
+{
+  "id": "chapter-testing-25",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-25",
+  "type": "Example",
+  "number": "1.12",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-26",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-26",
+  "type": "Example",
+  "number": "1.13",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-27",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-27",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   9. I am comfortable using various notations for derivatives.   "
+},
+{
+  "id": "chapter-testing-28",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-28",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   10. I can find derivatives using implicit differentitation.   Section 3.5: 5 - 22   "
+},
+{
+  "id": "chapter-testing-29",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-29",
+  "type": "Example",
+  "number": "1.14",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-30",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-30",
+  "type": "Example",
+  "number": "1.15",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-31",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-31",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   11. I can find derivatives using logarithmic differentiation.   Section 3.6: 45 - 56   "
+},
+{
+  "id": "chapter-testing-32",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-32",
+  "type": "Example",
+  "number": "1.16",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-33",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-33",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   12. I can determine when to use appropriate differentiation rules, including when to use the power, quotient, and chain rules, as well as using implicit and logarthmic differentiation.   Chapter 3 Review (p. 270): 1-20, 22-26, 28-33, 35-46, 48, 50, 53-54   "
+},
+{
+  "id": "chapter-testing-34",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-34",
+  "type": "Example",
+  "number": "1.17",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "chapter-testing-35",
+  "level": "2",
+  "url": "chapter-testing.html#chapter-testing-35",
+  "type": "Example",
+  "number": "1.18",
   "title": "",
   "body": "  "
 }
